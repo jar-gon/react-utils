501b15776dfe3d8663d856fb8ddbbaae5a1c18e7
billypon <billypon@gmail.com>
2019-10-30 14:07:13 +0800

    fix form module

--- src/form.tsx
+++ src/form.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Form, { FormComponentProps } from 'antd/es/form'
 import { GetFieldDecoratorOptions } from 'antd/es/form/Form'
 import { Dictionary } from '@billypon/ts-types'
-import { autobind } from '@billypon/react-decorator'
 
 import { Component } from './react'
 
@@ -16,6 +15,41 @@ export class FormComponent<P extends FormComponentProps = FormComponentProps, S 
   validFns: Dictionary<() => void> = { }
   inited: boolean
 
+  FormX = ({ children, ...props }) => {
+    return (
+      <Form { ...props }>
+        { children }
+        <button type="submit" hidden />
+      </Form>
+    )
+  }
+
+  FormItem = ({ name, label, children }) => {
+    return children && (
+      <Form.Item label={ label } validateStatus={ this.errors[name] && 'error' } help={ this.errors[name] }>
+        { this.fields[name](children) }
+      </Form.Item>
+    )
+  }
+
+  FormField = ({ name, children }) => {
+    return children && this.fields[name](children)
+  }
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        Object.keys(this.fields).forEach(id => this.errors[id] = null)
+        this.formSubmit(values)
+      } else {
+        Object.keys(this.fields).forEach(id => {
+          this.errors[id] = err[id] && err[id].errors.map(({ message }) => message)
+        })
+      }
+    })
+  }
+
   componentDidMount() {
     const { getFieldDecorator, validateFields } = this.props.form
     Object.entries(this.getFormFields()).forEach(([ id, options ]) => {
@@ -28,45 +62,6 @@ export class FormComponent<P extends FormComponentProps = FormComponentProps, S 
     })
     this.inited = true
     this.triggerUpdate().subscribe(() => this.formInit())
-  }
-
-  @autobind()
-  FormX({ children, ...props }): React.ReactNode {
-    return (
-      <Form { ...props }>
-        { children }
-        <button type="submit" hidden />
-      </Form>
-    )
-  }
-
-  @autobind()
-  FormItem({ name, label, children }): React.ReactNode {
-    return children && (
-      <Form.Item label={ label } validateStatus={ this.errors[name] && 'error' } help={ this.errors[name] }>
-        { this.fields[name](children) }
-      </Form.Item>
-    )
-  }
-
-  @autobind()
-  FormField({ name, children }): React.ReactNode {
-    return children && this.fields[name](children)
-  }
-
-  @autobind()
-  handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-    event.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        Object.keys(this.fields).forEach(id => this.errors[id] = null)
-        this.formSubmit(values)
-      } else {
-        Object.keys(this.fields).forEach(id => {
-          this.errors[id] = err[id] && err[id].errors.map(({ message }) => message)
-        })
-      }
-    })
   }
 
   getFormFields(): Dictionary<GetFieldDecoratorOptions> {
