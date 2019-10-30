af921a97f9411dc1f717372f77159ed5adb457f0
billypon <billypon@gmail.com>
2019-10-30 15:10:03 +0800

    fix next module

--- src/next.tsx
+++ src/next.tsx
@@ -4,19 +4,23 @@ import { NextComponentType } from 'next/dist/next-server/lib/utils'
 import { browser, redirectToLogin } from './common'
 
 export function withAuthentication(check: () => boolean) {
-  return (Component: NextComponentType) => {
+  return (Component: NextComponentType): any => {
     return class extends React.Component {
+      private logined: boolean
+
       constructor(props) {
         super(props)
         if (browser && !check()) {
           redirectToLogin()
+        } else {
+          this.logined = true
         }
       }
 
       static getInitialProps = Component.getInitialProps
 
       render() {
-        return browser ? <Component { ...this.props } /> : null
+        return this.logined ? <Component { ...this.props } /> : null
       }
     }
   }
